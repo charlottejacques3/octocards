ee33e4643f5a916ef95436bb938cd9da85898363
Charlotte Jacques <charlottej3333@gmail.com>
2026-04-14 16:31:37 -0700

    adding error 404 page

--- app/components/LoadingPage.tsx
+++ app/components/LoadingPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LoadingPage = () => {
   return (
-    <div className = 'w-full h-screen flex justify-center items-center'>
+    <div className='w-full h-screen flex justify-center items-center'>
       <h4>Loading...</h4>
     </div>
   )
---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Button from "./components/Button"
+
+const notFound = () => {
+  return (
+    <div className='w-full h-screen flex flex-col justify-center items-center'>
+      <h4 className='text-red-600 mb-2'>Page not found</h4>
+      <Button href='/'>Back to home</Button>
+    </div>
+  )
+}
+
+export default notFound
